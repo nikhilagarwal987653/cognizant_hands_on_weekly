633a6683b2e7e0a03b78f770601382d5a547f0dc
nikhil <nikhilagarwal987653@gmail.com>
2026-07-08 10:57:55 +0530

    Remove week-1 data from upskilling branch

--- week-1/Design-Pattern/FactoryMethodPatternExample/src/main/java/com/cognizant/factorymethod/Document.java
+++
@@ -1,5 +0,0 @@
-package com.cognizant.factorymethod;
-
-public interface Document {
-    void open();
-}
--- week-1/Design-Pattern/FactoryMethodPatternExample/src/main/java/com/cognizant/factorymethod/DocumentFactory.java
+++
@@ -1,5 +0,0 @@
-package com.cognizant.factorymethod;
-
-public abstract class DocumentFactory {
-    public abstract Document createDocument();
-}
