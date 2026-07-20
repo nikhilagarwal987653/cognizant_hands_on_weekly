233daede366dfc86c8cfffa1d09934d287fe6e80
nikhil <nikhilagarwal987653@gmail.com>
2026-07-20 13:05:02 +0530

    Implement Hello World REST service

--- week-3/Spring-learn/src/main/java/com/cognizant/Spring_learn/SpringLearnApplication.java
+++ week-3/Spring-learn/src/main/java/com/cognizant/Spring_learn/SpringLearnApplication.java
@@ -9,11 +9,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context= new ClassPathXmlApplicationContext("country.xml");
 
-		Country country=context.getBean("country",Country.class);
+
+		SpringApplication.run(SpringLearnApplication.class, args);
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+
+		Country country = context.getBean("country", Country.class);
 
 		System.out.println(country);
 	}
-
 }
