11095108a704fa035f343305ff427e1e0ffe0511
nikhil <nikhilagarwal987653@gmail.com>
2026-07-21 22:03:05 +0530

    Complete React HOL 3 score calculator app

---
+++ deepskilling/week-5/scorecalculatorapp/src/App.jsx
@@ -0,0 +1,14 @@
+import CalculateScore from "./Components/CalculateScore";
+
+function App() {
+  return (
+    <CalculateScore
+      Name="Nikhil Agarwal"
+      School="Delhi Public International School"
+      total={284}
+      goal={3}
+    />
+  );
+}
+
+export default App;
