31c274273a22b1bff29a854ca29217cee1d5df62
nikhil <nikhilagarwal987653@gmail.com>
2026-07-21 21:54:28 +0530

    Complete React HOL 2 StudentApp

---
+++ deepskilling/week-5/StudentApp/src/Components/About.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+class About extends Component{
+    render(){
+        return(
+            <h1>Welcome to the About Page of the Student Management Portal</h1>
+        );
+    }
+}
+export default About;
---
+++ deepskilling/week-5/StudentApp/src/Components/Contact.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+class Contact extends Component{
+    render(){
+        return(
+            <h1>Welcome to the Contact Page of the Student Management Portal</h1>
+        );
+    }
+}
+export default Contact;
