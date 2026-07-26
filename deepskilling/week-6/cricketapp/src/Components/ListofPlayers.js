d79eeb44cfc50d1711e7a4d36ffab3d4c72c6dfd
nikhil <nikhilagarwal987653@gmail.com>
2026-07-26 14:10:08 +0530

    Completed  React Exercise Cricket App

---
+++ deepskilling/week-6/cricketapp/src/Components/ListofPlayers.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ListofPlayers(props) {
+  return (
+    <ul>
+      {props.players.map((item, index) => (
+        <li key={index}>
+          Mr. {item.name} {item.score}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListofPlayers;
